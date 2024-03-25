56cbd57cf3d8933e35b81ff3ea31813dba766f05
afzal75 <afzalbhuiyan16@gmail.com>
2024-03-26 02:49:58 +0600

    navbar created successfully

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+
+const RegistrationPage = () => {
+    return (
+        <div>
+            <h2>Register form</h2>
+        </div>
+    );
+};
+
+export default RegistrationPage;
