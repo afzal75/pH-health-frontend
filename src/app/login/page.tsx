1deb2f69bf008b5f195cb542d350aec1db43e7d6
afzal75 <afzalbhuiyan16@gmail.com>
2024-04-03 02:53:28 +0600

    registration successfully completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,85 @@
+import { Container, Stack, Box, Typography, Grid, TextField, Button } from "@mui/material";
+import Image from "next/image";
+import assets from '@/assets'
+import Link from "next/link";
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Login Here</h1>
-        </div>
+        <Container>
+            <Stack sx={{
+                height: "100vh",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Box sx={{
+                    maxWidth: "600",
+                    width: "50%",
+                    boxShadow: 4,
+                    textAlign: "center",
+                    borderRadius: 1,
+                    p: 4
+                }}>
+                    <Stack
+                        sx={{
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}
+                    >
+                        <Box>
+                            <Image
+                                src={assets.svgs.logo}
+                                alt="logo"
+                                width={50}
+                                height={50}
+                            />
+                        </Box>
+                        <Box>
+                            <Typography variant="h6" fontWeight={600}>
+                                Login PH HelthCare
+                            </Typography>
+                        </Box>
+                    </Stack>
+                    <Box>
+                        <form>
+                            <Grid container spacing={2} my={2}>
+                                <Grid item md={6}>
+                                    <TextField
+                                        label="Email"
+                                        type="email"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                    />
+                                </Grid>
+                                <Grid item md={6}>
+                                    <TextField
+                                        label="Password"
+                                        type="password"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Typography
+                                textAlign="end"
+                                component="p"
+                                fontWeight={300}>
+                                Forgot Password?
+                            </Typography>
+                            <Button sx={{
+                                margin: "10px 0px"
+                            }} fullWidth={true}>Login
+                            </Button>
+                            <Typography component="p" fontWeight={300}>
+                                Don&apos; have an account?
+                                <Link href="/register"> Create an Account</Link>
+                            </Typography>
+                        </form>
+                    </Box>
+                </Box>
+            </Stack>
+        </Container>
     );
 };
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,160 @@
+"use client"
 
-const RegistrationPage = () => {
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import assets from '@/assets'
+import Link from "next/link";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { registerPatient } from "@/services/actions/registerPatient";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+
+interface IPatientData {
+    name: string;
+    email: string;
+    contactNumber: string;
+    address: string;
+}
+interface IPatientRegisterFormData {
+    password: string;
+    patient: IPatientData;
+}
+
+
+const RegisterPage = () => {
+    const router = useRouter()
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<IPatientRegisterFormData>()
+    const
+        onSubmit: SubmitHandler<IPatientRegisterFormData> = async (values) => {
+            const data = modifyPayload(values)
+
+            try {
+                const res = await registerPatient(data)
+                if (res?.data?.id) {
+                    toast.success(res.message);
+                    router.push("/login")
+                }
+            }
+            catch (err: any) {
+                console.error(err.message)
+            }
+        }
+
     return (
-        <div>
-            <h2>Register form</h2>
-        </div>
+        <Container>
+            <Stack
+                sx={{
+                    height: "100vh",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                <Box
+                    sx={{
+                        maxWidth: "600",
+                        width: "50%",
+                        boxShadow: 4,
+                        textAlign: "center",
+                        borderRadius: 1,
+                        p: 4
+                    }}
+                >
+                    <Stack
+                        sx={{
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}
+                    >
+                        <Box>
+                            <Image
+                                src={assets.svgs.logo}
+                                alt="logo"
+                                width={50}
+                                height={50}
+                            />
+                        </Box>
+                        <Box>
+                            <Typography variant="h6" fontWeight={600}>
+                                Patient Register
+                            </Typography>
+                        </Box>
+                    </Stack>
+                    <Box>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Grid container spacing={2} my={2}>
+                                <Grid item md={12}>
+                                    <TextField
+                                        label="Name"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                        {...register("patient.name")}
+                                    />
+                                </Grid>
+                                <Grid item md={6}>
+                                    <TextField
+                                        label="Email"
+                                        type="email"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                        {...register("patient.email")}
+                                    />
+                                </Grid>
+                                <Grid item md={6}>
+                                    <TextField
+                                        label="Password"
+                                        type="password"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                        {...register("password")}
+                                    />
+                                </Grid>
+                                <Grid item md={6}>
+                                    <TextField
+                                        label="Contact Number"
+                                        type="tel"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                        {...register("patient.contactNumber")}
+                                    />
+                                </Grid>
+                                <Grid item md={6}>
+                                    <TextField
+                                        label="Address"
+                                        type="text"
+                                        variant="outlined"
+                                        size="small"
+                                        fullWidth={true}
+                                        {...register("patient.address")}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button sx={{
+                                margin: "10px 0px"
+                            }} fullWidth={true}
+                                type="submit"
+                            >Register
+                            </Button>
+                            <Typography component="p" fontWeight={300}>
+                                Do you already have an account?
+                                <Link href="/login">Login</Link>
+                            </Typography>
+                        </form>
+                    </Box>
+                </Box>
+            </Stack>
+        </Container>
     );
 };
 
-export default RegistrationPage;
+export default RegisterPage;
